f7693bc045f64763361eefc7b748cea62bc9bbba
Roman Pena <roman.pena715@gmail.com>
2025-11-27 11:49:19 -0500

    added components folder, installed react-router, inital component creation for the music player

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,23 @@
+import { MusicPlayer } from "./components/MuiscPlayer"
+import { BrowserRouter, Routes, Route } from "react-router"
+
 function App() {
 
-  return <></>;
+  return (
+    <BrowserRouter>
+      <div className="app">
+        {/* <Navbar /> */}
+        <main className="app-main">
+          <div className="player-section">
+            <MusicPlayer />
+          </div>
+          <div className="content-section">
+
+          </div>
+        </main>
+      </div>
+    </BrowserRouter>
+  )
 }
 
 export default App
